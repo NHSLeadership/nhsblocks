3c61531d2fccf6e4fa2204c7f1f095bcb7c24b90
Belinda Mustoe <belinda@verytwisty.com>
2020-02-21 09:42:52 +0000

    Back and action links

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-element', 'wp-polyfill'), 'version' => '85a239b5ca3930545277d9b6cf81006f');
+<?php return array('dependencies' => array('lodash', 'wp-element', 'wp-polyfill'), 'version' => 'e47f563c8a513aa82f8f0b1bc7091fb0');
