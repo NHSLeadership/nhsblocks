6f0b2347e789b1a624e94d44514dec97e984044a
Marcin Calka <6230943+marcinca@users.noreply.github.com>
2020-09-16 13:23:09 +0100

    Add password for conference call

--- src/17-video-meetup/index.js
+++ src/17-video-meetup/index.js
@@ -7,133 +7,151 @@
 const {__} = wp.i18n;
 const {registerBlockType} = wp.blocks;
 const {RichText, InspectorControls} = wp.blockEditor;
+var userFulName = wp.data.select("core").getEntityRecords('user', 'user');
 
 var _wp$element = wp.element;
-var	Component = _wp$element.Component;
-var	Fragment = _wp$element.Fragment;
+var Component = _wp$element.Component;
+var Fragment = _wp$element.Fragment;
 
 var _wp$components = wp.components;
 var PanelBody = _wp$components.PanelBody;
 var TextControl = _wp$components.TextControl;
 var RangeControl = _wp$components.RangeControl;
+var defaultName = Math.random().toString(36).substring(2, 15);
+var defaultPassword = Math.random().toString(36).substring(2, 15);
 
 registerBlockType("nhsblocks/videomeetup", {
-	title: __("Video Meetup", "nhsblocks"),
-	category: "nhsblocks",
-	icon: "video-alt",
-	styles: [],
-	keywords: [__('jitsi', 'video-meetup'), __('jitsi', 'video-meetup')],
-	example: {
-		attributes: {
-			username: "Organiser",
-			roomname: Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5),
-			domainname: "meet.jit.si",
-			width: 700,
-			height: 500,
-		}
-	},
-	attributes: {
-		username: {
-			type: "string",
-			default: "Organiser"
-		},
-		roomname: {
-			type: "string",
-			default: "Sample Meetup",
-		},
-		domainname: {
-			type: "string",
-			default: "meet.jit.si"
-		},
-		width: {
-			type: 'number',
-			default: 700
-		},
-		height: {
-			type: 'number',
-			default: 500
-		}
-	},
-	supports: {
-		html: true
-	},
-	edit: function edit(props) {
-		const {
-			setAttributes
-		} = props;
-		var _props$attributes = props.attributes,
-			roomname = _props$attributes.roomname,
-			width = _props$attributes.width,
-			height = _props$attributes.height,
-			domainname = _props$attributes.domainname;
+    title: __("Video Meetup", "nhsblocks"),
+    category: "nhsblocks",
+    icon: "video-alt",
+    styles: [],
+    keywords: [__('jitsi', 'video-meetup'), __('jitsi', 'video-meetup')],
+    example: {
+        attributes: {
+            roomname: defaultName,
+            password: '',
+            domainname: "meet.jit.si",
+            width: 700,
+            height: 500,
+        }
+    },
+    attributes: {
+        domainname: {
+            type: "string",
+            default: "meet.jit.si"
+        },
+        roomname: {
+            type: 'string',
+            default: defaultName
+        },
+        password: {
+            type: 'string',
+            default: defaultPassword
+        },
+        width: {
+            type: 'number',
+            default: 700
+        },
+        height: {
+            type: 'number',
+            default: 500
+        }
+    },
+    supports: {
+        html: true
+    },
+    edit: function edit(props) {
+        const {
+            setAttributes
+        } = props;
+        var _props$attributes = props.attributes,
+            domainname = _props$attributes.domainname,
+            roomname = _props$attributes.roomname,
+            password = _props$attributes.password,
+            width = _props$attributes.width,
+            height = _props$attributes.height;
 
-		return wp.element.createElement(Fragment, null, wp.element.createElement(InspectorControls, null, wp.element.createElement(PanelBody, {
-			title: __('Settings'),
-			initialOpen: true
-		},
-		wp.element.createElement(TextControl, {
-			label: __('Jitsi Domain Name'),
-			value: domainname,
-			onChange: function onChange(val) {
-				return setAttributes({
-					domainname: val
-				});
-			}
-		}),
-		wp.element.createElement(TextControl, {
-			label: __('Room Name'),
-			value: roomname,
-			onChange: function onChange(val) {
-				return setAttributes({
-					roomname: val
-				});
-			}
-		}), wp.element.createElement(RangeControl, {
-			label: __('Screen Width'),
-			value: width,
-			onChange: function onChange(val) {
-				return setAttributes({
-					width: val
-				});
-			},
-			min: 50,
-			max: 2048,
-			step: 50
-		}), wp.element.createElement(RangeControl, {
-			label: __('Screen Height'),
-			value: height,
-			onChange: function onChange(val) {
-				return setAttributes({
-					height: val
-				});
-			},
-			min: 50,
-			max: 2048,
-			step: 50
-		}))), wp.element.createElement("div", {
-			className: "jitsi-wrapper",
-			"data-domain": domainname,
-			"data-room": roomname,
-			"data-width": width,
-			"data-height": height,
-			"data-user": 'Presenter'
-		}, (domainname + '/') . concat(domainname)));
-	},
-	save: function save(props) {
-		var _props$attributes = props.attributes,
-			username = _props$attributes.username,
-			roomname = _props$attributes.roomname,
-			width = _props$attributes.width,
-			height = _props$attributes.height,
-			domainname = _props$attributes.domainname;
+        return wp.element.createElement(Fragment, null, wp.element.createElement(InspectorControls, null, wp.element.createElement(PanelBody, {
+                title: __('Settings'),
+                initialOpen: true
+            },
+            wp.element.createElement(TextControl, {
+                label: __('Jitsi Domain Name'),
+                value: domainname,
+                onChange: function onChange(val) {
+                    return setAttributes({
+                        domainname: val
+                    });
+                }
+            }),
+            wp.element.createElement(TextControl, {
+                label: __('Room Name'),
+                value: roomname,
+                onChange: function onChange(val) {
+                    return setAttributes({
+                        roomname: val
+                    });
+                }
+            }),
+            wp.element.createElement(TextControl, {
+                label: __('Password'),
+                value: password,
+                onChange: function onChange(val) {
+                    return setAttributes({
+                        password: val
+                    });
+                }
+            }),
+            wp.element.createElement(RangeControl, {
+                label: __('Screen Width'),
+                value: width,
+                onChange: function onChange(val) {
+                    return setAttributes({
+                        width: val
+                    });
+                },
+                min: 50,
+                max: 2048,
+                step: 50
+            }),
+            wp.element.createElement(RangeControl, {
+                label: __('Screen Height'),
+                value: height,
+                onChange: function onChange(val) {
+                    return setAttributes({
+                        height: val
+                    });
+                },
+                min: 50,
+                max: 2048,
+                step: 50
+            }))), wp.element.createElement("div", {
+            className: "jitsi-wrapper",
+            "data-domain": domainname,
+            "data-room": roomname,
+            "data-pass": password,
+            "data-width": width,
+            "data-height": height,
+            "data-user": 'Presenter'
+        }, (domainname + '/').concat(roomname)));
+    },
+    save: function save(props) {
+        var _props$attributes = props.attributes,
+            username = _props$attributes.username,
+            roomname = _props$attributes.roomname,
+            password = _props$attributes.password,
+            width = _props$attributes.width,
+            height = _props$attributes.height,
+            domainname = _props$attributes.domainname;
 
-		return wp.element.createElement("div", {
-			className: "jitsi-wrapper",
-			"data-user": username,
-			"data-room": roomname,
-			"data-domain": domainname,
-			"data-width": width,
-			"data-height": height,
-		});
-	}
+        return wp.element.createElement("div", {
+            className: "jitsi-wrapper",
+            "data-domain": domainname,
+            "data-room": roomname,
+            "data-pass": password,
+            "data-width": width,
+            "data-height": height,
+            "data-user": username,
+        });
+    }
 });
--- src/17-video-meetup/jitsi.js
+++ src/17-video-meetup/jitsi.js
@@ -1,23 +1,39 @@
-jQuery(document).ready(function(){
-     const api = [];
+jQuery(document).ready(function () {
+    const api = [];
 
-     jQuery('.jitsi-wrapper').each(function(index, element) {
+    jQuery('.jitsi-wrapper').each(function (index, element) {
 
-         const domain = jQuery(element).data('domain'),
-             user = jQuery(element).data('user'),
-             room = jQuery(element).data('room'),
-             width = jQuery(element).data('width'),
-             height = jQuery(element).data('height');
-         // console.log("%c"+domain+"%c"+user+"%c"+room, "color:red;font-size:18px;");
+        const domain = jQuery(element).data('domain'),
+            user = (typeof displayname !== 'undefined') ? displayname : jQuery(element).data('user'),
+            room = jQuery(element).data('room'),
+            width = jQuery(element).data('width'),
+            height = jQuery(element).data('height'),
+            password = jQuery(element).data('pass');
 
         const options = {
             roomName: room,
             width: width,
             height: height,
-            parentNode:  document.querySelector('.jitsi-wrapper')
+            parentNode: document.querySelector('.jitsi-wrapper')
         };
 
         api[index] = new JitsiMeetExternalAPI(domain, options);
-        api[index].executeCommand('displayName', user);
-     });
+        if (user) {
+            api[index].executeCommand('displayName', user);
+        }
+
+        if (password !== '') {
+            // Set password for channel
+            api[index].addEventListener('participantRoleChanged', function (event) {
+                console.log('%cp: ' + password + event.role, ' color:red; font-size:20px;');
+                if (event.role === "moderator") {
+                    api[index].executeCommand('password', password);
+                }
+            });
+            // Join a protected channel
+            api[index].on('passwordRequired', function () {
+                api[index].executeCommand('password', password);
+            });
+        }
+    });
 });
