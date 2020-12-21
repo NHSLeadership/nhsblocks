a74f6062310edf27d4acc8af91a95629f220fe2f
Tony Blacker <tblacker7@gmail.com>
2020-12-21 13:11:16 +0000

    Three stage bugfix release:
     - 1 - corrected reveal block deprecated language
     - 2 - updated npm packages pulled in to prevent react version clash, stripped out unused packages
     - 3 - removed dependency on LearnDash module for button building

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-element', 'wp-polyfill'), 'version' => '84c3e480a91e11c5f169139a3d9b6b67');
+<?php return array('dependencies' => array('lodash', 'wp-element', 'wp-polyfill'), 'version' => '15ebe6a5d929b0c77dd9ec11fa921b50');
