6f9f97a665148d68edbbecfa67549b9d31638e82
Mahesh Murali P <maheshmuralip@gmail.com>
2022-05-30 14:16:25 +0100

    DI-4442 Experimental validation fix for Reveal block

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,2 @@
-<?php return array('dependencies' => array('lodash', 'wp-element'), 'version' => '9b51c7b36853f95c814c');
+
+<?php return array('dependencies' => array('lodash', 'wp-element', 'wp-polyfill'), 'version' => 'e9523f4d24e06d0a8313005f3a938b9d');
