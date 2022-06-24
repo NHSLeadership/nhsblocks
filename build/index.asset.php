08087d6c28625b0f1b0cc3016bf6e7dfb4ec0db0
Mahesh Murali P <maheshmuralip@gmail.com>
2022-06-24 13:05:13 +0100

    DI-4552 Experimental fix for Card Region for editors

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-element'), 'version' => '165f81543faefcd28d00');
+<?php return array('dependencies' => array('lodash', 'wp-element'), 'version' => 'a8d55743b611df00f509');
