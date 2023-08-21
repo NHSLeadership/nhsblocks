9a90cd9e136cfbec26e566ea7762235ce6b90946
Mahesh Murali P <maheshmuralip@gmail.com>
2023-08-21 12:59:52 +0100

    DI-5604 Fix simple reveals cant edit contents
    This basically reverts the commit added to hold html in reveal contents area.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-element'), 'version' => '8dd6fc4499c917411a72');
+<?php return array('dependencies' => array('lodash', 'wp-element'), 'version' => 'fa36f98533791ea412eb');
