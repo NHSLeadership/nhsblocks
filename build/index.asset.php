ff0d63320bde848b62a0d45822e89abb51bb2e20
Mahesh Murali P <mahesh.muralipoovampilly@nhs.net>
2025-04-25 10:21:46 +0100

    DI-7154 Security update (#186)

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react'), 'version' => '49619a2c5a6eff094434');
+<?php return array('dependencies' => array('lodash', 'react-jsx-runtime'), 'version' => 'd4cec148b032d467017e');
