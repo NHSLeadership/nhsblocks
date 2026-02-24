95ec41960bc4bc5827a8764e0c9d6956ea4e0b09
Mahesh Murali P <mahesh.muralipoovampilly@nhs.net>
2026-02-24 14:14:56 +0000

    Remove wp-components dep and rebuild assets
    Drop the unused 'wp-components' dependency from build/index.asset.php and update the asset version. Rebuild compiled assets (build/index.js) to reflect the source change, and include the corresponding edit to src/06-promo/index.js. This reduces declared dependencies and ensures the distributed bundle matches the updated source.

--- src/06-promo/index.js
+++ src/06-promo/index.js
@@ -2,10 +2,9 @@
  *  NHS Promo / Call out  Element
  *
  *  @reference: https://nhsuk.github.io/nhsuk-frontend/components/promo/index.html
- *  @author Tony Blacker, Mahesh Murali P - NHS Leadership Academy
- *  @version 1.1 30 Sep 2025
+ *  @author Tony Blacker, NHS Leadership Academy
+ *  @version 1.0 22nd July 2019
  */
-
 
 const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
@@ -69,10 +68,6 @@ registerBlockType('nhsblocks/promo1', {
 			selector: '.wp-block-nhsblocks-promo1 a.nhsuk-card__link',
 			attribute: 'href',
 		},
-		showChevron: {
-			type: 'boolean',
-			default: false,
-		},
 	},
 
 	edit: (props) => {
@@ -90,9 +85,9 @@ registerBlockType('nhsblocks/promo1', {
 		};
 
 		// Grab newPromoText, set the value of promoText to newPromoText.
-		// const onChangePromoText = (newPromoText) => {
-		// 	setAttributes({ promoText: newPromoText });
-		// };
+		const onChangePromoText = (newPromoText) => {
+			setAttributes({ promoText: newPromoText });
+		};
 		// Grab newPromoLink, set the value of promoLink to newPromoLink.
 		const onChangePromoLink = (newPromoLink) => {
 			setAttributes({ promoLink: newPromoLink });
@@ -103,99 +98,56 @@ registerBlockType('nhsblocks/promo1', {
 		const onImageSelect = (imageObject) => {
 			setAttributes({ promoImg: imageObject.sizes.podkitFeatImg.url });
 		};
-		const sanitizePromoText = (text) => text.replace(/<svg[\s\S]*?<\/svg>/gi, '');
+		const ALLOWED_BLOCKS = ['core/image'];
 
-		const onChangePromoText = (newPromoText) => {
-			setAttributes({ promoText: sanitizePromoText(newPromoText) });
-		};
-				const ALLOWED_BLOCKS = ['core/image'];
-
-		return ([
-			
-			<InspectorControls key="inspector-controls">
-				<PanelBody title={__('Promo Settings', 'nhsblocks')} initialOpen={true}>
-					<ToggleControl
-						label={__('Show Chevron Icon', 'nhsblocks')}
-						checked={!!props.attributes.showChevron}
-						onChange={(value) => setAttributes({ showChevron: value })}
-					/>
-				</PanelBody>
-			</InspectorControls>,
-
-			<ul key="promo-markup" className="nhsuk-grid-row nhsuk-card-group">
-				<li key={`promo-${props.clientId}`} className="nhsuk-grid-column-two-thirds nhsuk-card-group__item">
-					<div className={`${className} nhsuk-card nhsuk-card--clickable`}>
-						<InnerBlocks allowedBlocks={ALLOWED_BLOCKS} />
-						<div className="nhsuk-card__content nhsuk-card__content--primary">
-							<h2 className="nhsuk-card__heading nhsuk-heading-m">
-								<URLInputButton
-									className="nhsblocks-dropdown__input"
-									label={__('Panel Link', 'nhsblocks')}
-									onChange={onChangePromoLink}
-									url={promoLink}
-								/>
-								<RichText
-									placeholder={__('Promo Title', 'nhsblocks')}
-									value={promoTitle}
-									onChange={onChangePromoTitle}
-								/>
-							</h2>
-							<div className="nhsuk-card__description">
-								<RichText
-									{...blockProps}
-									placeholder={__('Promo Contents', 'nhsblocks')}
-									onChange={onChangePromoText}
-									value={promoText}
-								/>
-								{props.attributes.showChevron && (
-									<svg className="nhsuk-icon" xmlns="http://www.w3.org/2000/svg" width="27" height="27" aria-hidden="true" focusable="false">
-										<circle cx="13.333" cy="13.333" r="13.333" fill="" />
-										<g data-name="Group 1" fill="none" stroke="#fff" strokeLinecap="round" strokeMiterlimit="10" strokeWidth="2.667">
-											<path d="M15.438 13l-3.771 3.771" />
-											<path data-name="Path" d="M11.667 9.229L15.438 13" />
-										</g>
-									</svg>
-								)}
-							</div>
-						</div>
+		return (
+			<div className={`${className} nhsuk-card nhsuk-card--clickable`}>
+				<InnerBlocks allowedBlocks={ALLOWED_BLOCKS} />
+				<div className="nhsuk-card__content">
+					<h2 className="nhsuk-card__heading nhsuk-heading-m">
+						<URLInputButton
+							className="nhsblocks-dropdown__input"
+							label={__('Panel Link', 'nhsblocks')}
+							onChange={onChangePromoLink}
+							url={promoLink}
+						/>
+						<RichText
+							placeholder={__('Promo Title', 'nhsblocks')}
+							value={promoTitle}
+							onChange={onChangePromoTitle}
+						/>
+					</h2>
+					<div className="nhsuk-card__description">
+						<RichText
+							{ ...blockProps }
+							placeholder={__('Promo Contents', 'nhsblocks')}
+							onChange={onChangePromoText}
+							value={promoText}
+						/>
 					</div>
-				</li>
-			</ul>
-		]);
+				</div>
+			</div>
+		);
 	},
 	save: (props) => {
 		const blockProps = useBlockProps.save();
 		const {
 			attributes: { promoTitle, promoText, promoLink },
 		} = props;
-
 		return (
-			<ul {...blockProps} className="nhsuk-grid-row nhsuk-card-group">
-				<li key={`promo-${props.clientId}`} className="nhsuk-grid-column-two-thirds nhsuk-card-group__item">
-					<div className="nhsuk-card nhsuk-card--clickable">
-						<InnerBlocks.Content />
-						<div className="nhsuk-card__content nhsuk-card__content--primary">
-							<h2 className="nhsuk-card__heading nhsuk-heading-m">
-								<a href={promoLink} className="nhsuk-card__link">
-									<RichText.Content value={promoTitle} />
-								</a>
-							</h2>
-							<div className="nhsuk-card__description">
-								<RichText.Content value={promoText} />
-								{props.attributes.showChevron && (
-									<svg className="nhsuk-icon" xmlns="http://www.w3.org/2000/svg" width="27" height="27" aria-hidden="true" focusable="false">
-										<circle cx="13.333" cy="13.333" r="13.333" fill="" />
-										<g data-name="Group 1" fill="none" stroke="#fff" strokeLinecap="round" strokeMiterlimit="10" strokeWidth="2.667">
-											<path d="M15.438 13l-3.771 3.771" />
-											<path data-name="Path" d="M11.667 9.229L15.438 13" />
-										</g>
-									</svg>
-								)}
-							</div>
-						</div>
+			<div className="nhsuk-card nhsuk-card--clickable">
+				<InnerBlocks.Content />
+				<div className="nhsuk-card__content">
+					<h2 className="nhsuk-card__heading nhsuk-heading-m">
+						<a href={promoLink} className="nhsuk-card__link">
+							<RichText.Content value={promoTitle} />
+						</a>
+					</h2>
+					<div className="nhsuk-card__description">
+						<RichText.Content { ...blockProps } value={promoText} />
 					</div>
-				</li>
-			</ul>
+				</div>
+			</div>
 		);
 	},
 	deprecated: [
@@ -277,98 +229,5 @@ registerBlockType('nhsblocks/promo1', {
 				</div>
 			),
 		},
-		{
-			attributes: {
-				promoTitle: {
-					type: 'string',
-					source: 'html',
-					selector: '.wp-block-nhsblocks-promo1 .nhsuk-card__link',
-				},
-				promoText: {
-					type: 'string',
-					source: 'html',
-					selector: '.wp-block-nhsblocks-promo1 .nhsuk-card__description',
-				},
-				promoLink: {
-					type: 'string',
-					source: 'attribute',
-					selector: '.wp-block-nhsblocks-promo1 a.nhsuk-card__link',
-					attribute: 'href',
-				},
-			},
-			save: (props) => {
-				const blockProps = useBlockProps.save();
-				const {
-					attributes: { promoTitle, promoText, promoLink },
-				} = props;
-
-				return (
-					<div className="nhsuk-card nhsuk-card--clickable">
-						<InnerBlocks.Content />
-						<div className="nhsuk-card__content">
-							<h2 className="nhsuk-card__heading nhsuk-heading-m">
-								<a href={promoLink} className="nhsuk-card__link">
-									<RichText.Content value={promoTitle} />
-								</a>
-							</h2>
-							<div className="nhsuk-card__description">
-								<RichText.Content {...blockProps} value={promoText} />
-							</div>
-						</div>
-					</div>
-				);
-			},
-		},
-
-		{
-			attributes: {
-				promoTitle: {
-					type: 'string',
-					source: 'html',
-					selector: '.wp-block-nhsblocks-promo1 .nhsuk-card__link',
-				},
-				promoText: {
-					type: 'string',
-					source: 'html',
-					selector: '.wp-block-nhsblocks-promo1 .nhsuk-card__description',
-				},
-				promoLink: {
-					type: 'string',
-					source: 'attribute',
-					selector: '.wp-block-nhsblocks-promo1 a.nhsuk-card__link',
-					attribute: 'href',
-				},
-				showChevron: {
-					type: 'boolean',
-					default: false,
-				},
-			},
-			save: (props) => {
-				const blockProps = useBlockProps.save();
-				const {
-					attributes: { promoTitle, promoText, promoLink },
-				} = props;
-
-				return (
-					<ul className="nhsuk-grid-row nhsuk-card-group">
-						<li className="nhsuk-grid-column-two-thirds nhsuk-card-group__item">
-							<div className="nhsuk-card nhsuk-card--clickable">
-								<InnerBlocks.Content />
-								<div className="nhsuk-card__content nhsuk-card__content--primary">
-									<h2 className="nhsuk-card__heading nhsuk-heading-m">
-										<a href={promoLink} className="nhsuk-card__link">
-											<RichText.Content value={promoTitle} />
-										</a>
-									</h2>
-									<div className="nhsuk-card__description">
-										<RichText.Content {...blockProps} value={promoText} />
-									</div>
-								</div>
-							</div>
-						</li>
-					</ul>
-				);
-			},
-		}
 	],
 });
